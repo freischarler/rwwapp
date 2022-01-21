741ddf8f7d0a47d82ad4f4f522e4470dfb74f061
Freischarler <martin.paz@live.com.ar>
2022-01-21 12:40:09 -0300

    Refactor

--- views/view.go
+++ views/view.go
@@ -1,11 +1,18 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"net/http"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".gohtml"
+)
 
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/boostrap.gohtml",
-		"views/layouts/footer.gohtml")
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
@@ -19,4 +26,16 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
+
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
 }
